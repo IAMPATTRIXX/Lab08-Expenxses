929ea93f03d279a7eba25f39ce7eb17fcf2fe018
IAMPATTRIXX <dexza53@gmail.com>
2020-03-21 01:15:58 +0700

    1

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src={"https://firebasestorage.googleapis.com/v0/b/lab08-expensesv2.appspot.com/o/70435832_2439526586123197_514942355174850560_o.jpg?alt=media&token=4a18066a-6dbe-4917-bfe1-490c5288740d"} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
             <p>Siwakon nak-Yim (610610705)</p>
